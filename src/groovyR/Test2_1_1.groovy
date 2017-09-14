e3e7585c39290aa6e49280cbe73b4c5efc81144f
wuxiangyu <wuyu012@126.com>
2017-09-14 20:38:41 +0800

    groovy去掉方法和类名，完全可以共用java代码

--- src/groovyR/Test2_1_1.groovy
+++ src/groovyR/Test2_1_1.groovy
@@ -1,10 +1,17 @@
 package groovyR
 
-class Test2_1_1 {
-    public static void main(String[] args) {
+// 1. 去掉main和类定义，这样就变成了一个srcipt了
+//class Test2_1_1 {
+//    public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             System.out.print("ho ");
         }
         System.out.print("Merry Groovy!");
-    }
+//    }
+//}
+
+//2. 实现效果和上面的一致
+for (i in 0..2) {
+    print "ho"
 }
+print 'Merry Groovy!'
