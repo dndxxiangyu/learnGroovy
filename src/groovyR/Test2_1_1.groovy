7202f13eaa91c298e79feb070ae27da83b2af3a4
wuxiangyu <wuyu012@126.com>
2017-09-14 17:20:06 +0800

    两份一模一样的groovy和java代码，groovy完全兼容java。

---
+++ src/groovyR/Test2_1_1.groovy
@@ -0,0 +1,10 @@
+package groovyR
+
+class Test2_1_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("ho ");
+        }
+        System.out.print("Merry Groovy!");
+    }
+}
---
+++ src/javaR/Test2_1_1.java
@@ -0,0 +1,10 @@
+package javaR;
+
+public class Test2_1_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("ho ");
+        }
+        System.out.print("Merry Groovy!");
+    }
+}
