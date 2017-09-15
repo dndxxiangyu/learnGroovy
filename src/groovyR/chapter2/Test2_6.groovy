9cdca8fc8138557ba50f477da98cc8475862b230
wuxiangyu <wuyu012@126.com>
2017-09-15 16:23:30 +0800

    groovy设置监听器：as，如果有多个方法，放入一个map中，key为方法名。

---
+++ src/groovyR/chapter2/Test2_6.groovy
@@ -0,0 +1,49 @@
+package groovyR.chapter2
+
+
+class Test2_6 {
+    public static void main(String[] args) {
+        ButtonJava buttonJava = new ButtonJava()
+//        buttonJava.addActionListener(new ButtonJava.ActionListener() {
+//            @Override
+//            void actionPerformed() {
+//                println "click button"
+//            }
+//        })
+
+        //groovy
+        buttonJava.addActionListener(
+                { println "$it click button" } as ButtonJava.ActionListener
+        )
+
+        //方法名作为key
+        buttonJava.addActionListener(
+                [
+                        click: {println "click button"},
+                        longClick: {println "long click button"}
+                ] as ButtonJava.ActionListener
+        )
+        buttonJava.longClick("wxy1")
+        buttonJava.click("wxy2")
+    }
+}
+
+class ButtonJava {
+    public void addActionListener(ActionListener listener) {
+        this.listener = listener;
+    }
+    private ActionListener listener;
+    public interface ActionListener {
+        void click(String who);
+
+        void longClick(String who)
+
+    }
+    public void click(String whoClick) {
+        listener?.click(whoClick);
+    }
+
+    public void longClick(String who) {
+        listener?.longClick(who)
+    }
+}
