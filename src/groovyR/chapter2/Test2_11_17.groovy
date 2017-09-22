72dd33511e58e5b47153db20e93bd299285242a9
wuxiangyu <wuyu012@126.com>
2017-09-22 11:14:40 +0800

    闭包的例子。

--- src/groovyR/chapter2/Test2_11_17.groovy
+++ src/groovyR/chapter2/Test2_11_17.groovy
@@ -2,7 +2,26 @@ package groovyR.chapter2
 
 int[] arr = [1,2,3,4,5]
 def arr1 = [1,2,3,4,5] //默认创建的是ArrayList
-print arr.getClass()
+println arr.getClass()
+println arr[2]
 println arr1.getClass()
-
+println arr1.get(2)
 def arr3 = [1,2,3,4,5] as int[]
+
+closureA = {
+    a = 10;
+    b = 11;
+    a + b;
+}
+println closureA()
+
+//闭包参数
+def toTriple = { n ->
+    n * n
+}
+println toTriple(10)
+
+closureB = {
+    println 'closureB'
+}
+closureB
