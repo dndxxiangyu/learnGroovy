4f6285c8082e4ce2a57c72d68ff7f16843739d80
wuxiangyu <wuyu012@126.com>
2017-09-27 11:38:31 +0800

    使用闭包的方式有3种，有一种比较奇葩：把闭包定义内联到函数体中。

--- src/groovyR/chapter4/Test4_3.groovy
+++ src/groovyR/chapter4/Test4_3.groovy
@@ -16,6 +16,7 @@ println "total of even numbers from 1 to 10 is"
  * 在闭包中，最后一行的返回值return也是可选的。
  */
 println totalSelectValues(10) {it % 2 == 0}
+println totalSelectValues(10, {it % 2 == 0})
 
 def isOdd = { it % 2 != 0 }
 println totalSelectValues(10, isOdd)
