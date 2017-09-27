e385ca23e73c518575681c0df60b52f3b023df00
wuxiangyu <wuyu012@126.com>
2017-09-27 14:12:08 +0800

    自定义闭包资源清理。

--- src/groovyR/chapter4/Test4_5.groovy
+++ src/groovyR/chapter4/Test4_5.groovy
@@ -6,9 +6,55 @@ writer.write("!")
 /**
  * 闭包进行资源清理
  */
-new FileWriter("output.txt").withWriter (){
+new FileWriter("output.txt").withWriter() {
     writer -> writer.write("a")
 }
 new FileWriter("output.txt").withWriter {
     writer -> writer.write("a")
+}
+
+/**
+ * 在任何实例调用之前先调用open，完成时在调用close
+ */
+class Resource {
+    def open() {
+        println "opened..."
+    }
+
+    def close() {
+        println "closed"
+    }
+
+    def read() {
+        println "read...."
+    }
+
+    def write() {
+        println "write..."
+    }
+    /**
+     * 这个静态方法，
+     * @param closure
+     * @return
+     */
+    def static use(closure) {
+        def r = new Resource();
+        try {
+            r.open()
+            closure(r)
+        } finally {
+            r.close()
+        }
+    }
+}
+
+def resource = new Resource()
+resource.open()
+resource.read()
+resource.write()// forgot close
+
+
+Resource.use { res ->
+    res.read()
+    res.write()
 }
